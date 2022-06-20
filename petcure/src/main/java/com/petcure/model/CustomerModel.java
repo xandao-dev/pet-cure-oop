91127591e70b29d9c962fae5866a02ab8c89f3d9
Alexandre Calil <alexandrecalilmf@gmail.com>
2022-06-20 15:32:34 -0300

    refactor: remove redundant interface

--- petcure/src/main/java/com/petcure/model/CustomerModel.java
+++ petcure/src/main/java/com/petcure/model/CustomerModel.java
@@ -1,10 +1,8 @@
 package com.petcure.model;
 
-import java.io.Serializable;
 import java.util.ArrayList;
 
-public class CustomerModel extends PersonModel implements Serializable {
-	private static final long serialVersionUID = 1L;
+public class CustomerModel extends PersonModel {
 	private int debt = 0;
 	private ArrayList<AnimalModel> pets = new ArrayList<>();
 
